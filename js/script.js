e18510aaab11b983e9062f6cc542fd7f1fac60a0
Jimmy D <jd9192016@gmail.com>
2019-03-29 12:45:18 -0400

    second upload
    Random color generator applied to body bacground
    set interval

--- js/script.js
+++ js/script.js
@@ -3,14 +3,16 @@ let year = document.getElementsByClassName("year")[0];
 
 const quotes = [
   {quote : "Don't cry because it's over, smile because it happened.",
-    source : "Dr. Seuss "},
+    source : "Dr. Seuss "
+  },
  {
    quote:"Be yourself; everyone else is already taken.",
    source:"Oscar Wilde "
  },
  {
    quote:"Be the change that you wish to see in the world.",
-   source:"Mahatma Gandhi "
+   source:"Mahatma Gandhi ",
+   tag:"Wisdom"
  },
  {
    quote:"Always forgive your enemies; nothing annoys them so much.",
@@ -29,7 +31,8 @@ const quotes = [
  {
   quote:"Love all, trust a few, do wrong to none.",
   source:"William Shakespeare",
-  citation:"As You Like It"
+  citation:"As You Like It",
+  tag: "Comedy"
  },
  {
   quote:"Never put off till tomorrow what may be done day after tomorrow just as well.",
@@ -37,7 +40,8 @@ const quotes = [
  },
  {
   quote:"That which does not kill us makes us stronger.",
-  source:" Friedrich Nietzsche "
+  source:" Friedrich Nietzsche ",
+  tag:"Social"
  },
  {
   quote:"Being deeply loved by someone gives you strength, while loving someone deeply gives you courage.",
@@ -53,6 +57,7 @@ const quotes = [
    quote:"Do not be anxious about tomorrow, for tomorrow will be anxious for itself. Let the day's own trouble be sufficient for the day.",
    source:"Jesus",
    citation:"Matthew 6:34",
+   tag:"Religious",
    year: 'AD 27–29'
  }
 ];
@@ -63,14 +68,43 @@ const quotes = [
               the quotes array allowing us to generate a quote based on 
               the random quote
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
 let getRandomQuote = ()=>{
   let i= Math.floor(Math.random()*12);
   return quotes[i];
 }
+/*================================================================================================
+        Generates Random numbers from 0 to 255 that will be used to get random RGB color 
+ ==================================================================================================*/
+let randomNumber = function() {
+  let randomNumber= Math.floor(Math.random()*255);
+   return randomNumber;
+ }
 
+ 
+ let getRandomColor=()=>{
+     let color = '';
+     color+=`rgb(`;
+     color+= randomNumber();
+     color+= `,`;
+     color+= randomNumber();
+     color+= `,`;
+     color+= randomNumber();
+     color+=`)`;
+     return color;
+ };
 
-console.log(getRandomQuote());
+/*================================================================================================
+            set background color for the body and the button in the page 
+ ==================================================================================================*/
+ let setBackgroundColor = ()=>  {
+   let color = getRandomColor();
+  let body = document.getElementsByTagName("body")[0];
+  body.style.backgroundColor=color ;
+  document.getElementById('loadQuote').style.backgroundColor=color;
+ }
 
+ 
 let printQuote = ()=>{
   let randomQuote = getRandomQuote();
   let quoteBox = document.getElementById('quote-box');
@@ -82,10 +116,15 @@ let printQuote = ()=>{
   }if(randomQuote.year){
     quoteBoxHtml += `<span class="year"> ${randomQuote.year} </span>`; 
   } 
+  if(randomQuote.tag){
+    quoteBoxHtml += `<br><span class="tag"> ${randomQuote.tag} </span>`; 
+  } 
   quoteBoxHtml += `</p>`
-  console.log( quoteBoxHtml);
   quoteBox.innerHTML= quoteBoxHtml;
-}
-printQuote();
+  setBackgroundColor();
+  }
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+
+window.setInterval(printQuote, 20000);
